f92f69f57fb891bd36df2d31633c2350c94ee308
Karan Trivedi <katrivedi420@gmail.com>
2025-03-21 11:14:52 +0000

    variables and datatypes

---
+++ Basic/datatypes.js
@@ -0,0 +1,21 @@
+"use strict";  // treats all JS code as newer version
+
+// alert(3+3)  // This will give error as we are using NodeJs not Browser
+
+console.log(3+5)
+console.log("Hello")
+
+let val = null  // standalone value and also a datatype 
+
+// undefined =>   value is not assigned yet
+// int => range   2 to power 53 Then comes  BigINT
+// string 
+// boolean 
+// symbol => mostly used for uniqueness
+
+// object  
+
+console.log(typeof null);   //   it will give Object
+console.log(typeof undefined);  // it will give undefined
+
+
---
+++ Basic/variables.js
@@ -0,0 +1,13 @@
+const acc = 6969
+let namee = 'karan'
+var rollno = 69
+state = "gujrat"
+
+/*
+we always use let because of issue in block scope and functional scope
+*/
+
+// acc = 2  //  we are not allowed to do this
+
+console.table([acc,namee,rollno,state])   //  new style of printing, it prints in form of table
+// console.log(acc);
